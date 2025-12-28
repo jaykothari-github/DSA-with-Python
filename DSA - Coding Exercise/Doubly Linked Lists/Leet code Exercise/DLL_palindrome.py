42067114f461a08c6d51c2e5ca2fd2a33f2438c8
Jay Kothari <jaykothari49@gmail.com>
2025-12-28 23:43:11 +0530

    adding DLL leed code exercise

---
+++ DSA - Coding Exercise/Doubly Linked Lists/Leet code Exercise/DLL_palindrome.py
@@ -0,0 +1,96 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # WRITE IS_PALINDROME METHOD HERE #
+    #                                 #
+    #                                 #
+    #                                 #
+    #                                 #
+    ###################################
+
+    def is_palindrome(self):
+        start = self.head
+        end = self.tail
+
+        ##### Easy and full interation method #####
+
+        # while start:
+        #     if start.value != end.value:      # checking start and end value
+        #         return False                  # if it is not same then return false
+        #     start = start.next                # go to next step of start
+        #     end = end.prev                    # come one step back from end
+        # return True                           # if all good then return true
+
+        ############################################
+
+        ###### Based on length we limit iteration in half #####
+
+        for i in range(self.length // 2):       # running loop based on length
+            if start.value != end.value:            
+                return False
+            start = start.next
+            end = end.prev
+        return True
+        
+        ##########################################################
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print( my_dll_1.is_palindrome() )
+
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print( my_dll_2.is_palindrome() )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll_1 is_palindrome:
+    True
+
+    my_dll_2 is_palindrome:
+    False
+
+"""
+
